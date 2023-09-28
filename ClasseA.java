a7fc39142d7e1003c656e85829d96b1a30da9ca8
Rhuan C. Borges <97767859+rhuancborges@users.noreply.github.com>
2023-09-28 11:57:58 -0300

    upload classe A

---
+++ ClasseA.java
@@ -0,0 +1,28 @@
+public class ClasseA{
+  private int A1;
+  private float A2;
+
+  public void setA1(int A1){
+    this.A1=A1;
+  }
+
+  public void setA2(float A2){
+    this.A2=A2;
+  }
+
+  public int getA1(){
+    return A1;
+  }
+
+  public float getA2(){
+    return A2;
+  }
+
+  public void MA1(){
+    System.out.println("Este e o metodo MA1");
+  }
+
+  public void MA2(){
+    System.out.prinln("Este e o metodo MA2");
+  }
+}
