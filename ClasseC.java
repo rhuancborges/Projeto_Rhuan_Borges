4ff87afef79800183c601410328f306ccfc9b1d8
Rhuan <rhucambor@gmail.com>
2023-09-28 13:40:40 -0300

    "Criação da Classe C"

---
+++ ClasseC.java
@@ -0,0 +1,12 @@
+public class ClasseC {
+    private String C1;
+    private int C2;
+
+    public void MC1(){
+        System.out.println("Este e o metodo MC1");
+    }
+
+    public void MC2(){
+        System.out.println("Este e o metodo MC2");
+    }
+}
