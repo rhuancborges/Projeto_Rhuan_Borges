9315eab885afb30281fcf35fe669444ea06cd6d2
Rhuan C. Borges <97767859+rhuancborges@users.noreply.github.com>
2023-09-28 12:01:23 -0300

    upload Classe B

---
+++ ClasseB.java
@@ -0,0 +1,28 @@
+public class ClasseB{
+  private int B1;
+  private float B2;
+
+  public void setB1(int B1){
+    this.B1=B1;
+  }
+
+  public void setB2(float B2){
+    this.B2=B2;
+  }
+
+  public int getB1(){
+    return B1;
+  }
+
+  public float getB2(){
+    return B2;
+  }
+
+  public void MB1(){
+    System.out.println("Este e o metodo MB1");
+  }
+
+  public void MB2(){
+    System.out.prinln("Este e o metodo MB2");
+  }
+}
