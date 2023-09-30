212c1e81da02ef33fa82d19325520203e074a69e
Rhuan <rhucambor@gmail.com>
2023-09-30 11:35:04 -0300

    Adicionando ClasseD

---
+++ ClasseD.java
@@ -0,0 +1,12 @@
+public class ClasseD {
+    private int D1;
+    private String D2;
+
+    public void MD1(){
+        System.out.println("Este e o metodo MD1");
+    }
+
+    public void MD2(){
+        System.out.println("Este e o metodo MD2 ");
+    }
+}
